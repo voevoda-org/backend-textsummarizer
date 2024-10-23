a207ba82e7e660bf98227a751a14355ffa7c2428
Christopher Werner <christopher.werner1@gmx.net>
2024-10-23 15:33:34 +0200

    Improve logging.

--- src/main/kotlin/textsummarizer/plugins/Monitoring.kt
+++ src/main/kotlin/textsummarizer/plugins/Monitoring.kt
@@ -4,6 +4,9 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.plugins.calllogging.processingTimeMillis
+import io.ktor.server.plugins.origin
+import io.ktor.server.request.httpMethod
 import io.ktor.server.request.path
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
@@ -48,8 +51,40 @@ fun Application.configureMonitoring() {
     install(CallLogging) {
         level = Level.INFO
         filter { call ->
+            call.request.path().startsWith("/api/v1")
+        }
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
             val path = call.request.path()
-            path.startsWith("/api/v1/") && !path.startsWith("/metrics")
+            val queryParams =
+                call.request.queryParameters
+                    .entries()
+                    .joinToString(", ") { "${it.key}=${it.value}" }
+            val duration = call.processingTimeMillis()
+            val remoteHost = call.request.origin.remoteHost
+            val coloredStatus =
+                when {
+                    status == null -> "\u001B[33mUNKNOWN\u001B[0m"
+                    status.value < 300 -> "\u001B[32m$status\u001B[0m"
+                    status.value < 400 -> "\u001B[33m$status\u001B[0m"
+                    else -> "\u001B[31m$status\u001B[0m"
+                }
+            val coloredMethod = "\u001B[36m$httpMethod\u001B[0m"
+            """
+        |
+        |------------------------ Request Details ------------------------
+        |Status: $coloredStatus
+        |Method: $coloredMethod
+        |Path: $path
+        |Query Params: $queryParams
+        |Remote Host: $remoteHost
+        |User Agent: $userAgent
+        |Duration: ${duration}ms
+        |------------------------------------------------------------------
+        |
+  """.trimMargin()
         }
     }
 
